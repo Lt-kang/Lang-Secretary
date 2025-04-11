f01bc53b36e07dc3269cb262ed022678ec3d8ac7
Seung Hyun <k_penrose@naver.com>
2025-04-11 14:34:22 +0900

    update

--- app/config.py
+++ app/config.py
@@ -6,9 +6,11 @@ load_dotenv()
 
 # 환경 변수 읽기
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-VECTOR_DB_PATH = os.getenv("VECTOR_DB_PATH", "./db/default_vector.db")
-NORMAL_DB_PATH = os.getenv("NORMAL_DB_PATH", "./db/default_normal.db")
-ENV = os.getenv("ENV", "development")
+VECTOR_DB_PATH = os.getenv("VECTOR_DB_PATH")
+NORMAL_DB_PATH = os.getenv("NORMAL_DB_PATH")
+ENV = os.getenv("ENV")
+
+
 
 # # LangChain에서 사용할 LLM 관련 기본 설정도 여기에 모을 수 있음
 # DEFAULT_TEMPERATURE = 0.7
---
+++ app/services/run_graph.py
@@ -0,0 +1,9 @@
+from app.chains.graph_builder import build_graph
+
+
+GRAPH = build_graph()
+
+def run_main_flow(input_text: str) -> str:
+    inputs = {"input": input_text}
+    outputs = GRAPH.invoke(inputs)
+    return outputs["output"]
