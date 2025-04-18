dfa7e9948f0656d44f2e22982bd226d62bb7ba3d
Seung Hyun <114465217+Lt-kang@users.noreply.github.com>
2025-04-19 05:29:11 +0900

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 백엔드 (FastAPI) 실행 (백그라운드)
-uvicorn src.run_api:app --host 0.0.0.0 --port 8000 &
-
-# 프론트엔드 (Streamlit) 실행
-streamlit run frontend/run_streamlit.py --server.port=8080 --server.address=0.0.0.0
