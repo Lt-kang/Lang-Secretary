3230c43fb5ca16633833e39a21b6b08ebfb02f7a
Xenrose <k_penrose@naver.com>
2025-04-26 23:13:19 +0900

    input update

--- src/schema/agent_input.py
+++ src/schema/agent_input.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 paper
 '''
 class PaperInput(BaseModel):
-    paper_info: str
+    paper_info: str = Field(description="사용자 입력 혹은 arxiv url 예시: https://arxiv.org/abs/2210.03629")
 
 
 
@@ -14,7 +14,7 @@ class PaperInput(BaseModel):
 weather
 '''
 class WeatherInput(BaseModel):
-    city: str
+    city: str = Field(description="도시 이름 예시: Seoul", default="Seoul")
 
     
 class ClothesInput(BaseModel):
