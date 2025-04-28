8bd781b3c130be9ca3579701de97bbcecb41b0b4
Seung Hyun <k_penrose@naver.com>
2025-04-28 16:53:42 +0900

    update prompts template

--- src/core/prompts.py
+++ src/core/prompts.py
@@ -1,12 +1,9 @@
 from langchain.prompts import (
     ChatPromptTemplate,
-    SystemMessagePromptTemplate, 
-    HumanMessagePromptTemplate
 )
 
 
-categorize_system_message = SystemMessagePromptTemplate.from_template(
-    '''
+categorize_system_message = '''
     다음 문장이 어떤 유형에 더 유사한지 골라주세요.
 
     답변은 반드시 단어로만 해주세요. [날씨, 논문, 기타]
@@ -15,23 +12,16 @@ categorize_system_message = SystemMessagePromptTemplate.from_template(
     선택에 대한 힌트를 줄게
     arxiv.org/ 이와 같은 url을 입력 받았다면 이는 논문으로 분류하면 돼
     '''
-)
 
-categorize_human_message = HumanMessagePromptTemplate.from_template(
-    '''
-    {input}
-    '''
-)
 
 CATEGORIZE_PROMPT = ChatPromptTemplate.from_messages([
-    categorize_system_message,
-    categorize_human_message
+    ("system", categorize_system_message.strip()),
+    ("human", "{input}")
 ])
 
 
 
-paper_system_message = SystemMessagePromptTemplate.from_template(
-    '''
+paper_system_message = '''
     1. "system_message"는 네가 반드시 지켜야할 사항이니 만약 system_message가 있다면 반드시 참고해.
     2. <CHAT_HISTORY_START>와 <CHAT_HISTORY_END> 사이에 있는 대화들은 전부 너와 내가 함께 나눈 대화니까. 기억해두고 앞으로 대화할 때 참고해서 답변해줘
     3. <CHAT_HISTORY_START>와 <CHAT_HISTORY_END>는 INPUT 가장 처음에만 등장하니 이후에 등장하는 CHAT_HISTORY는 무시해
@@ -49,17 +39,10 @@ paper_system_message = SystemMessagePromptTemplate.from_template(
 
     절대 Action과 Final Answer를 동시에 출력하지 마세요.
     절대 Action 없이 일반 문장만 출력하지 마세요.
+    '''
 
-    '''
-)
-
-paper_human_message = HumanMessagePromptTemplate.from_template(
-    '''
-    {input}
-    '''
-)
 
 PAPER_PROMPT = ChatPromptTemplate.from_messages([
-    paper_system_message,
-    paper_human_message
+    ("system", paper_system_message.strip()),
+    ("human", "{input}")
 ])
